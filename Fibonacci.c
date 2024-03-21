476d6ef593e501d92230b5cd8305f222d449619f
kmh9550599@gmail.com <kmh9550599@gmail.com>
2024-03-21 09:42:11 +0900

    초기 커밋.

---
+++ Fibonacci.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int Fibonacci_rec(int n);
+
+int main() {
+	int fibonacci = 0;
+	fibonacci = Fibonacci_rec(6);
+	printf("Fibonacci 6 = %d\n", fibonacci);
+}
+
+int Fibonacci_rec(int n) {
+	printf("f(%d) ", n);
+	if (n == 0) return 0;
+	else if (n == 1) return 1;
+	else return(Fibonacci_rec(n - 1) + Fibonacci_rec(n - 2));
+}
